201c3c6d03c44ff82494669b1111240c3bea693e
Penny Chase <mpchase@mac.com>
2025-02-09 18:45:38 -0500

    Terminate idle servers

--- todo/lib/todo/server.ex
+++ todo/lib/todo/server.ex
@@ -1,6 +1,8 @@
 defmodule Todo.Server do
   use GenServer, restart: :temporary
 
+  @expiry_idle_timeout :timer.seconds(10)
+
   # Client API
   def start_link(name) do
     GenServer.start_link(__MODULE__, name, name: via_tuple(name))
@@ -35,29 +37,34 @@ defmodule Todo.Server do
 
   def handle_continue(:init, {name, nil}) do
     todo_list = Todo.Database.get(name) || Todo.List.new()
-    {:noreply, {name, todo_list}}
+    {:noreply, {name, todo_list}, @expiry_idle_timeout}
+  end
+
+  def handle_info(:timeout, {name, todo_list}) do
+    IO.puts("Stopping todo server: #{name}")
+    {:stop, :normal, {name, todo_list}}  
   end
 
   def handle_call({:entries, date}, _, {name, todo_list}) do
-    {:reply, Todo.List.entries(todo_list, date), {name, todo_list}}
+    {:reply, Todo.List.entries(todo_list, date), {name, todo_list}, @expiry_idle_timeout}
   end
 
   def handle_cast({:add_entry, new_entry}, {name, todo_list}) do
     new_list = Todo.List.add_entry(todo_list, new_entry)
     Todo.Database.store(name, new_list)
-    {:noreply, {name, new_list}}
+    {:noreply, {name, new_list}, @expiry_idle_timeout}
   end
 
   def handle_cast({:delete_entry, entry_id}, {name, todo_list}) do
     new_list = Todo.List.delete_entry(todo_list, entry_id)
     Todo.Database.store(name, new_list)
-    {:noreply, {name, new_list}}
+    {:noreply, {name, new_list}, @expiry_idle_timeout}
   end
 
   def handle_cast({:update_entry, entry_id, field_name, new_value}, {name, todo_list}) do
     new_list = Todo.List.update_entry(todo_list, entry_id, field_name, new_value)
     Todo.Database.store(name, new_list)
-    {:noreply, {name, new_list}}
+    {:noreply, {name, new_list}, @expiry_idle_timeout}
   end
 
 end
