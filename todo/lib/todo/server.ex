192c4f32bb199f612ff06b5644c2c806e73e1ac8
Penny Chase <mpchase@mac.com>
2025-02-09 15:58:12 -0500

    Changed Todo Server to Task

--- todo/lib/todo/server.ex
+++ todo/lib/todo/server.ex
@@ -1,63 +1,50 @@
 defmodule Todo.Server do
-  use GenServer, restart: :temporary
+  use Agent, restart: :temporary
 
   # Client API
   def start_link(name) do
-    GenServer.start_link(__MODULE__, name, name: via_tuple(name))
+    Agent.start_link(
+      fn -> 
+        IO.puts("Starting todo server: ${name}")
+        {name, Todo.Database.get(name) || Todo.List.new()}
+      end,
+      name: via_tuple(name) 
+    )
   end
 
   def entries(todo_server, date) do
-    GenServer.call(todo_server, {:entries, date})
+    Agent.get(todo_server, fn {_name, todo_list} ->
+      Todo.List.entries(todo_list, date)
+    end)
   end
 
   def add_entry(todo_server, new_entry) do
-    GenServer.cast(todo_server, {:add_entry, new_entry})
+    Agent.cast(todo_server, fn {name, todo_list} ->
+      new_list = Todo.List.add_entry(todo_list, new_entry)
+      Todo.Database.store(name, new_list)
+      {name, new_list}
+    end)
   end
 
   def delete_entry(todo_server, entry_id) do
-    GenServer.cast(todo_server, {:delete_entry, entry_id})
+    Agent.cast(todo_server, fn {name, todo_list} ->
+      new_list = Todo.List.delete_entry(todo_list, entry_id)
+      Todo.Database.store(name, new_list)
+      {name, new_list}
+    end)
   end
 
   def update_entry(todo_server, entry_id, field_name, new_value) do
-    GenServer.cast(todo_server, {:update_entry, entry_id, field_name, new_value})
+    Agent.cast(todo_server, fn {name, todo_list} ->
+      new_list = Todo.List.update_entry(todo_list, entry_id,field_name, new_value)
+      Todo.Database.store(name, new_list)
+      {name, new_list}
+    end)
   end
 
   # Utilities
   defp via_tuple(name) do
     Todo.ProcessRegistry.via_tuple({__MODULE__, name})
-  end
-
-  # Callbacks
-  def init(name) do
-    IO.puts("Starting todo server: #{name}")
-    {:ok, {name, nil}, {:continue, :init} }
-  end
-
-  def handle_continue(:init, {name, nil}) do
-    todo_list = Todo.Database.get(name) || Todo.List.new()
-    {:noreply, {name, todo_list}}
-  end
-
-  def handle_call({:entries, date}, _, {name, todo_list}) do
-    {:reply, Todo.List.entries(todo_list, date), {name, todo_list}}
-  end
-
-  def handle_cast({:add_entry, new_entry}, {name, todo_list}) do
-    new_list = Todo.List.add_entry(todo_list, new_entry)
-    Todo.Database.store(name, new_list)
-    {:noreply, {name, new_list}}
-  end
-
-  def handle_cast({:delete_entry, entry_id}, {name, todo_list}) do
-    new_list = Todo.List.delete_entry(todo_list, entry_id)
-    Todo.Database.store(name, new_list)
-    {:noreply, {name, new_list}}
-  end
-
-  def handle_cast({:update_entry, entry_id, field_name, new_value}, {name, todo_list}) do
-    new_list = Todo.List.update_entry(todo_list, entry_id, field_name, new_value)
-    Todo.Database.store(name, new_list)
-    {:noreply, {name, new_list}}
   end
 
 end
