834933a6c7687382cd779ac44736a47eadcabd34
Penny Chase <mpchase@mac.com>
2025-02-09 22:09:19 -0500

    Key Value suning GenServe and ETS

---
+++ key_value/lib/key_value.ex
@@ -0,0 +1,36 @@
+defmodule KeyValue do
+  use GenServer
+
+  # Client API
+
+  def start_link do
+    GenServer.start_link(__MODULE__, nil, name: __MODULE__)
+  end
+
+  def put(key, value) do
+    GenServer.cast(__MODULE__, {:put, key, value})
+  end
+
+  def get(key) do
+    GenServer.call(__MODULE__, {:get, key})
+  end
+
+  # Callbacks
+
+  @impl GenServer
+  def init(_) do
+    {:ok, %{}}
+  end
+
+  @impl GenServer
+  def handle_cast({:put, key, value}, store) do
+    {:noreply, Map.put(store, key, value)}
+  end
+
+  @impl GenServer
+  def handle_call({:get, key}, _, store) do
+    {:reply, Map.get(store, key), store}    
+  end
+
+
+end
