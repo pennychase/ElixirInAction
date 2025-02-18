8357dab38bb6f5c0667f216b788d981a54e650d1
Penny Chase <mpchase@mac.com>
2025-02-18 18:30:30 -0500

    Todo.Cache changes for distributed application (global registration)

--- todo/lib/todo/cache.ex
+++ todo/lib/todo/cache.ex
@@ -12,9 +12,20 @@ defmodule Todo.Cache do
   end
 
   def server_process(todo_list_name) do
-    case start_child(todo_list_name) do
-      {:ok, pid} -> pid
-      {:error, {:already_started, pid}} -> pid
+    existing_process(todo_list_name) || new_process(todo_list_name)
+  end
+
+  defp existing_process(todo_list_name) do
+    Todo.Server.whereis(todo_list_name)
+  end
+
+  def new_process(todo_list_name) do
+    case DynamicSupervisor.start_child(
+      __MODULE__,
+      {Todo.Server, todo_list_name}
+    ) do
+        {:ok, pid} -> pid
+        {:error, {:aready_started, pid}} -> pid
     end
   end
 
